53b679146a4d9b9ca008cd8a1c2c97b0aef2d909
zbh123456hui <18737374397@163.com>
2023-08-21 14:00:25 +0800

    main

--- mkfifo/mkfifo_r.cpp
+++ mkfifo/mkfifo_r.cpp
@@ -1,1 +1,12 @@
 #include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define P_FIFO "/tmp/p_fifo"
+
+int main(int argc,char **argv){
+    
+}
