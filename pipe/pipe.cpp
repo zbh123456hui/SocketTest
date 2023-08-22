2bf9018ea227f87abb92815c17bf17b39672cc69
zbh123456hui <18737374397@163.com>
2023-08-22 14:32:21 +0800

    main

--- pipe/pipe.cpp
+++ pipe/pipe.cpp
@@ -13,4 +13,23 @@ int main(){
     int returned_count;
     /*创建无名管道*/
     pipe(fd);
+    /*创建子进程*/
+    pid=fork();
+    if(pid<0){
+        printf("Error in fork\n");
+        exit(1);
+    }else if(pid==0){   /*执行子进程*/
+        printf("in the child process...\n");
+        /*子进程向父进程写数据，关闭管道的读端*/
+        close(fd[INPUT]);
+        write(fd[OUTPUT],"hello world",strlen("hello world"));
+        exit(0);
+    }else{   /*执行父进程*/
+        printf("in the parent process...\n");
+        /*父进程从管道读取子进程写的程序，关闭管道的写端*/
+        close(fd[OUTPUT]);
+        returned_count=read(fd[INPUT],buf,sizeof(buf));
+        printf("%d bytes of data received from child process: %s\n",returned_count,buf);
+    }
+    return 0;
 }
